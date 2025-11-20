5fbde33cbd8ab61ed4aab7b4eb055a8258825984
louhc <louhancheng@outlook.com>
2025-11-20 15:51:44 +0800

    Add d_open

--- subroutines/src/pcs/ligesis/mod.rs
+++ subroutines/src/pcs/ligesis/mod.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 use crate::{pcs::prelude::*, IOPProof, PolyIOP, SumCheck};
 use arithmetic::{math::Math, VPAuxInfo, VirtualPolynomial};
 use ark_ff::{BigInteger, PrimeField};
@@ -423,7 +425,7 @@ impl<F: PrimeField> PolynomialCommitmentScheme<F> for LigeSISPCS<F> {
         } = advice;
 
         // Step 1
-        let (z1, z2): (Vec<F>, Vec<F>) = (point[log_n..].to_vec(), point[..log_n].to_vec());
+        let (z1, z2) = (point[log_n..].to_vec(), point[..log_n].to_vec());
         let eq_z1 = get_tensor(&z1);
 
         // Step 2
@@ -686,6 +688,374 @@ impl<F: PrimeField> PolynomialCommitmentScheme<F> for LigeSISPCS<F> {
             // commitment proofs
             deepfold_batched_proof,
         })
+    }
+
+    fn d_open(
+        prover_param: impl Borrow<Self::ProverParam>,
+        poly: &Self::Polynomial,
+        advice: &Self::ProverCommitmentAdvice,
+        point: &Self::Point,
+        transcript: &mut IOPTranscript<F>,
+    ) -> Result<Option<Self::Proof>, PCSError> {
+        // trim parameters
+        let &LigeSISProverParam {
+            eta,
+            s_lambda,
+            mu,
+            log_m,
+            log_n,
+            rs_len,
+            c,
+            ref rs,
+            ref mat_a,
+            ref mat_a_pad,
+            ref com_mat_a_advice,
+            ref deepfold_prover_param,
+        } = prover_param.borrow();
+        let num_party = Net::n_parties();
+        let num_party_vars = Net::n_parties().log_2() as usize;
+        let log_m = log_m - num_party_vars;
+        let (m, n) = (1 << log_m, 1 << log_n);
+        let log_rs_len = rs_len.ilog2() as usize;
+
+        assert_eq!(mu, log_m + log_n + num_party_vars);
+        assert_eq!(poly.num_vars, mu);
+
+        let mat_f = reshape(&poly.evaluations, m, n);
+
+        let LigeSISProverCommitmentAdvice {
+            mat_f_prime,
+            mat_h,
+            mat_h_pad,
+            com_mat_h_advice,
+        } = advice;
+
+        // Step 1
+        let (z1, z2) = (point[log_n..].to_vec(), point[..log_n].to_vec());
+        let (z1_0, z1_1) = (
+            z1[log_m - num_party_vars..].to_vec(),
+            z1[..log_m - num_party_vars].to_vec(),
+        );
+        let eq_z1_1 = get_tensor(&z1_1);
+        let eq_z1_0 = get_tensor(&z1_0);
+
+        // Step 2
+        let a_k = (0..n)
+            .map(|j| (0..m / num_party_vars).map(|i| eq_z1_1[i] * mat_f[i][j]).sum())
+            .collect::<Vec<F>>();
+        let a_k_list = Net::send_to_master(&a_k).unwrap();
+        let a = if Net::am_master() {
+            (0..n).map(|j| (0..num_party).map(|k| eq_z1_0[k] * a_k_list[k][j]).sum()).collect::<Vec<F>>()
+        } else {
+            vec![]
+        };
+        let a_pad = evals_to_arcpoly(&resize_eval(&a, deepfold_prover_param.max_mu));
+        let (com_a, com_a_advice) = if Net::am_master() {
+            DeepFoldPCS::commit(deepfold_prover_param, &a_pad)?
+        } else {
+            (DeepFoldCommitment::default(), DeepFoldProverCommitmentAdvice::default())
+        };
+
+        // Step 3
+        let I = if Net::am_master() {
+            let I = transcript.get_and_append_challenge_indices(b"I", s_lambda, 2 * n)?;
+            let msg = (0..num_party).map(|_| I.clone()).collect::<Vec<_>>();
+            Net::recv_from_master(Some(msg));
+            I
+        } else {
+            Net::recv_from_master(None)
+        };
+
+        // Step 4
+        let mat_f_prime_trans = transposition(&mat_f_prime);
+        let mat_bI_k = transposition(
+            &I.iter()
+                .map(|&i| decompose_vector(&mat_f_prime_trans[i]))
+                .collect::<Vec<_>>(),
+        );
+        let mat_bI_k_list = Net::send_to_master(&mat_bI_k).unwrap();
+        let mat_bI = if Net::am_master() { mat_bI_k_list.concat() } else { vec![] };
+        let bI_field = bool_vec_to_field_vec(&mat_bI.concat());
+        let bI_field_pad = evals_to_arcpoly(&resize_eval(&bI_field, deepfold_prover_param.max_mu));
+        let (com_bI, com_bI_advice) =
+            DeepFoldPCS::commit(deepfold_prover_param, &evals_to_arcpoly(&bI_field))?;
+        
+        // Step 5
+        let (alpha1, alpha2, alpha3) = if Net::am_master() {
+            let alpha1 = transcript
+            .get_and_append_challenge_vectors(b"alpha1", (m * eta * s_lambda).ilog2() as usize)?;
+            let alpha2 = transcript.get_and_append_challenge_vectors(b"alpha2", c.ilog2() as usize)?;
+            let alpha3 = transcript.get_and_append_challenge_vectors(b"alpha3", log_rs_len)?;
+            let msg = (0..num_party)
+                .map(|_| (alpha1.clone(), alpha2.clone(), alpha3.clone()))
+                .collect::<Vec<_>>();
+            Net::recv_from_master(Some(msg));
+            (alpha1, alpha2, alpha3)
+        } else {
+            Net::recv_from_master(None)
+        };
+
+        // Step 6 (non-disbtributed)
+
+        let (bI_check_proof, r1) = if Net::am_master() {
+            let mut bI_check = VirtualPolynomial::new(bI_field.len().ilog2() as usize);
+            bI_check
+                .add_mle_list(
+                    [
+                        evals_to_arcpoly(&bI_field),
+                        evals_to_arcpoly(&bI_field.iter().map(|&x| x - F::ONE).collect::<Vec<F>>()),
+                        evals_to_arcpoly(&get_tensor(&alpha1)),
+                    ],
+                    F::ONE,
+                )
+                .unwrap();
+            let bI_check_proof = <PolyIOP<F> as SumCheck<F>>::prove(bI_check, transcript).unwrap();
+            let r1 = bI_check_proof.point.clone();
+            (bI_check_proof, r1)
+        } else {
+            (IOPProof::<F>::default(), vec![])
+        };
+
+        // Step 7
+        let (rs_a, rs_a_pad, com_rs_a, com_rs_a_advice, rs_a_check_proof, r6, a, mat_g_check_proofs) = if Net::am_master() {
+            let rs_a = rs.encode(&a);
+            let g = rs.get_generator();
+            let rs_a_pad = evals_to_arcpoly(&resize_eval(&rs_a, deepfold_prover_param.max_mu));
+            let (com_rs_a, com_rs_a_advice) = DeepFoldPCS::commit(deepfold_prover_param, &rs_a_pad)?;
+
+            // Step 7.1 check eq_alpha3^T * G * a = eq_alpha3^T * rs_a
+            //  \sum_i alpha3_mat_g(i) * a(i) = eq_alpha3^T * rs_a
+            // reduce to alpha3_mat_g(r6), a(r6)
+            let alpha3_mat_g = compute_alpha_mat_g(log_rs_len as usize, log_n, &g, &alpha3);
+            let mut rs_a_check = VirtualPolynomial::new(log_n);
+            rs_a_check
+                .add_mle_list(
+                    [
+                        evals_to_arcpoly(&alpha3_mat_g[log_rs_len][..n].to_vec()),
+                        evals_to_arcpoly(&a),
+                    ],
+                    F::ONE,
+                )
+                .unwrap();
+            let rs_a_check_proof = <PolyIOP<F> as SumCheck<F>>::prove(rs_a_check, transcript).unwrap();
+            let r6 = rs_a_check_proof.point.clone();
+            // Step 7.2 check alpha3_mat_g(r6)
+            let mut cur_p = vec![r6.clone(), vec![F::ZERO; log_rs_len - log_n]].concat();
+            let mut mat_g_check_proofs = Vec::new();
+            for i in (2..=log_rs_len).rev() {
+                let (x, b) = (cur_p[..i - 1].to_vec(), cur_p[i - 1]);
+                let gi = g.pow([1u64 << (log_rs_len - i)]);
+                let w = (0..1 << (i - 1))
+                    .map(|z| {
+                        F::ONE - alpha3[log_rs_len - i]
+                            + alpha3[log_rs_len - i]
+                                * (gi.pow([z]) * (F::ONE - b) + gi.pow([z + (1 << (i - 1))]) * b)
+                    })
+                    .collect::<Vec<_>>();
+                let mut mat_g_check = VirtualPolynomial::new(i - 1);
+                mat_g_check
+                    .add_mle_list(
+                        [
+                            evals_to_arcpoly(&get_tensor(&x)),
+                            evals_to_arcpoly(&alpha3_mat_g[i - 1]),
+                            evals_to_arcpoly(&w),
+                        ],
+                        F::ONE,
+                    )
+                    .unwrap();
+                let mat_g_check_proof =
+                    <PolyIOP<F> as SumCheck<F>>::prove(mat_g_check, transcript).unwrap();
+
+                cur_p = mat_g_check_proof.point.clone();
+                mat_g_check_proofs.push(mat_g_check_proof);
+            }
+            (rs_a, rs_a_pad, com_rs_a, com_rs_a_advice, rs_a_check_proof, r6, a, mat_g_check_proofs)
+        } else {
+            (
+                vec![],
+                Arc::default(),
+                DeepFoldCommitment::default(),
+                DeepFoldProverCommitmentAdvice::default(),
+                IOPProof::<F>::default(),
+                vec![],
+                vec![],
+                vec![],
+            )
+        };
+
+        // Step 8
+        let (lookup_proof, r2, r3, v) = if Net::am_master() {
+            let eq_alpha2_a_bI = mat_mul(
+                &vec![get_tensor(&alpha2)],
+                &field_mat_mul_bool_mat(&mat_a, &mat_bI),
+            )
+            .concat();
+            let v = otimes(
+                &get_tensor(&z1),
+                &(0..eta)
+                    .map(|i| F::from(2u64).pow([i as u64]))
+                    .collect::<Vec<_>>(),
+            );
+            let v_bI = field_mat_mul_bool_mat(&vec![v.clone()], &mat_bI).concat();
+            let eq_alpha2_h = mat_mul(&vec![get_tensor(&alpha2)], &mat_h).concat();
+
+            // Lookup Argument for (I, eq_alpha2_a_bI, v_bI) in ([2n], eq_alpha2_h, rs_a)
+            let gamma = transcript.get_and_append_challenge_vectors(b"gamma", 1)?[0];
+            let r = transcript.get_and_append_challenge_vectors(b"r", s_lambda.ilog2() as usize)?;
+            let eq_r = get_tensor(&r);
+
+            let mut lookup_check = VirtualPolynomial::new(s_lambda.ilog2() as usize);
+            let mut eq_alpha2_h_circ_I = vec![F::ZERO; s_lambda];
+            let mut rs_a_circ_I = vec![F::ZERO; s_lambda];
+            for i in 0..s_lambda {
+                eq_alpha2_h_circ_I[i] = eq_alpha2_h[I[i]];
+                rs_a_circ_I[i] = rs_a[I[i]];
+            }
+
+            lookup_check
+                .add_mle_list(
+                    [evals_to_arcpoly(&eq_r), evals_to_arcpoly(&eq_alpha2_a_bI)],
+                    F::ONE,
+                )
+                .unwrap();
+            lookup_check
+                .add_mle_list(
+                    [
+                        evals_to_arcpoly(&eq_r),
+                        evals_to_arcpoly(&eq_alpha2_h_circ_I),
+                    ],
+                    -F::ONE,
+                )
+                .unwrap();
+            lookup_check
+                .add_mle_list([evals_to_arcpoly(&eq_r), evals_to_arcpoly(&v_bI)], gamma)
+                .unwrap();
+            lookup_check
+                .add_mle_list(
+                    [evals_to_arcpoly(&eq_r), evals_to_arcpoly(&rs_a_circ_I)],
+                    -gamma,
+                )
+                .unwrap();
+
+            let lookup_proof = LigeSISLookupProof {
+                sumcheck_proof: <PolyIOP<F> as SumCheck<F>>::prove(lookup_check, transcript).unwrap(),
+            };
+
+            let r2 = vec![F::ONE; s_lambda.ilog2() as usize];
+            let r3 = vec![F::ONE; 1 + log_n];
+            (lookup_proof, r2, r3, v)
+        } else {
+            (
+                LigeSISLookupProof { sumcheck_proof: IOPProof::<F>::default() }, 
+                vec![], 
+                vec![],
+                vec![],
+            )
+        };
+
+        // Step 9
+        let (bI_r2, alpha2_a_bI_r2_check_proof, r4) = if Net::am_master() {
+            let alpha2_a = mat_mul(&vec![get_tensor(&alpha2)], &mat_a)[0].clone();
+            let bI_r2 =
+                field_mat_mul_bool_mat(&vec![get_tensor(&r2)], &transposition(&mat_bI))[0].clone();
+            let mut alpha2_a_bI_r2_check = VirtualPolynomial::new(mat_bI.len().ilog2() as usize);
+            alpha2_a_bI_r2_check
+                .add_mle_list(
+                    [evals_to_arcpoly(&alpha2_a), evals_to_arcpoly(&bI_r2)],
+                    F::ONE,
+                )
+                .unwrap();
+            let alpha2_a_bI_r2_check_proof =
+                <PolyIOP<F> as SumCheck<F>>::prove(alpha2_a_bI_r2_check, transcript).unwrap();
+            let r4 = alpha2_a_bI_r2_check_proof.point.clone();
+            (bI_r2, alpha2_a_bI_r2_check_proof, r4)
+        } else {
+            (vec![], IOPProof::<F>::default(), vec![])
+        };
+
+        // Step 10
+        let (v_bI_r2_check_proof, r5) = if Net::am_master() {
+            let mut v_bI_r2_check = VirtualPolynomial::new(v.len().ilog2() as usize);
+            v_bI_r2_check
+                .add_mle_list([evals_to_arcpoly(&v), evals_to_arcpoly(&bI_r2)], F::ONE)
+                .unwrap();
+            let v_bI_r2_check_proof =
+                <PolyIOP<F> as SumCheck<F>>::prove(v_bI_r2_check, transcript).unwrap();
+            let r5 = v_bI_r2_check_proof.point.clone();
+            (v_bI_r2_check_proof, r5)
+        } else {
+            (IOPProof::<F>::default(), vec![])
+        };
+
+        // Step 11
+        if Net::am_master() {
+            let polys = [
+                &a_pad,
+                &a_pad,
+                &rs_a_pad,
+                &rs_a_pad,
+                &mat_h_pad,
+                &mat_a_pad,
+                &bI_field_pad,
+                &bI_field_pad,
+                &bI_field_pad,
+            ]
+            .map(|p| Arc::clone(p))
+            .to_vec();
+            let advices = [
+                &com_a_advice,
+                &com_a_advice,
+                &com_rs_a_advice,
+                &com_rs_a_advice,
+                &com_mat_h_advice,
+                &com_mat_a_advice,
+                &com_bI_advice,
+                &com_bI_advice,
+                &com_bI_advice,
+            ];
+            let points = [
+                &z2,
+                &r6,
+                &r3,
+                &alpha3,
+                &vec![r3.clone(), alpha2.clone()].concat(),
+                &vec![r4.clone(), alpha2.clone()].concat(),
+                &r1,
+                &vec![r2.clone(), r4.clone()].concat(),
+                &vec![r2.clone(), r5.clone()].concat(),
+            ]
+            .map(|p| resize_point(p, deepfold_prover_param.max_mu));
+            let evals = [F::ZERO; 9];
+
+            let deepfold_batched_proof = DeepFoldPCS::multi_open(
+                deepfold_prover_param,
+                polys,
+                &advices,
+                &points,
+                &evals,
+                transcript,
+            )?;
+
+            Ok(Some(LigeSISProof {
+                // commitments
+                com_a,
+                com_bI,
+                com_rs_a,
+                // sumcheck proofs
+                bI_check_proof,
+                alpha2_a_bI_r2_check_proof,
+                v_bI_r2_check_proof,
+                rs_a_check_proof,
+                mat_g_check_proofs,
+                // lookup proof
+                lookup_proof,
+                // commitment proofs
+                deepfold_batched_proof,
+            }))
+        } else {
+            Ok(None)
+        }
+        
     }
 
     fn verify(
